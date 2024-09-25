7a98394d503a0cdc9bb2af398f0978adea77106b
Aman Rajput <amrajput@expediagroup.com>
2024-09-25 17:02:21 +0530

    Added the test API

--- src/main/kotlin/com/techorgx/api/controllers/IdentityController.kt
+++ src/main/kotlin/com/techorgx/api/controllers/IdentityController.kt
@@ -1,7 +1,13 @@
 package com.techorgx.api.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class IdentityController {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "Testing the API"
+    }
 }
