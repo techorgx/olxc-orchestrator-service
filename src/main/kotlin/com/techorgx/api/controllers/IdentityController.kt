ea5a45c87ed8fd63616148eb19456a081b5477b9
Aman Rajput <amrajput@expediagroup.com>
2024-09-25 20:22:05 +0530

    Updated gitignore and added login dummy api

--- src/main/kotlin/com/techorgx/api/controllers/IdentityController.kt
+++ src/main/kotlin/com/techorgx/api/controllers/IdentityController.kt
@@ -1,15 +1,16 @@
 package com.techorgx.api.controllers
 
 import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @CrossOrigin(origins = ["http://localhost:5173"])
 class IdentityController {
 
-    @GetMapping("/test")
-    fun test(): String {
-        return "Testing the API"
+    @PostMapping("/login")
+    fun getLoginCredentials(@RequestBody request: String): String {
+        return "Login credentials: $request"
     }
 }
